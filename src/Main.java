258acd6559309b6bb9565f4a9fec9b9d43011348
AdrianCo97 <adrian.corazza@outlook.com>
2021-03-12 18:00:52 +0100

    Started working on a switch case menu that is used to navigate the app

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
 import java.util.ArrayList;
+import java.util.Scanner;
+
 
 public class Main {
+
+	static ArrayList<Recipe> recipes = new ArrayList<Recipe>();
+	
+	public static void menu(String choice) {
+		Scanner input = new Scanner(System.in);
+		switch (choice) {
+		case "Add":
+			System.out.println("What is the name of the dish?:");
+			String name = input.nextLine();
+			
+			System.out.println("What are the ingredients?:");
+			ArrayList<String> ingredients = new ArrayList<String>();
+			
+		}
+	}
 
 	public static void main(String[] args) {
 
