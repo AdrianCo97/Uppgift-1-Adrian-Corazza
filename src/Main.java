5db4c065c941c64af18fb9ebf3d19ff5e5b96191
AdrianCo97 <adrian.corazza@outlook.com>
2021-03-14 17:45:55 +0100

    Added some new recipes to the arraylist

--- src/Main.java
+++ src/Main.java
@@ -17,6 +17,39 @@ public class Main {
     System.out.println(pancakes);
     
     pancakes.getIngredients();
+    
+    //-------------------------------------------------------------
+    
+    String[] pastaRecipe = {"spaghetti", "Oregano", "Mozarella"};
+	Recipe pasta = new Recipe("pasta", pastaRecipe , 30);	
+	
+	recipes.add(pasta);
+    
+    System.out.println(pasta);
+    
+    pasta.getIngredients();
+    
+    //-------------------------------------------------------------
+    
+    String[] potatoGratinRecipe= {"potatoes", "cooking cream", "Cheese"};
+	Recipe potatoGratin = new Recipe("pasta", potatoGratinRecipe , 60);	
+	
+	recipes.add(potatoGratin);
+    
+    System.out.println(potatoGratin);
+    
+    potatoGratin.getIngredients();
+    
+    //-------------------------------------------------------------
+    
+    String[] carbonaraRecipe = {"spaghetti", "Bacon", "Egg"};
+	Recipe carbonara = new Recipe("Spaghetti Carbonare", carbonaraRecipe , 28);	
+	
+	recipes.add(carbonara);
+    
+    System.out.println(carbonara);
+    
+    carbonara.getIngredients();
 
 	}
 }
